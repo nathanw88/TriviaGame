26f4c64df11170e14734151badef3a2de49d9d62
Nate <33041144+nathanw88@users.noreply.github.com>
2018-11-16 15:25:59 -0600

    updated apps.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+var counter = 0;
 function start() {
   var queryurl =
     "https://opentdb.com/api.php?amount=10&category=19&type=multiple";
@@ -5,21 +6,24 @@ function start() {
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    <p id="timer">time remaining</p>
-      var newDiv = $("<div>");
-  //         <p>Question?</p>
-  //         <br />
-  //         <br />
-  //         <section class="answer" id="answer-1">answer</section>
-  //         <br />
-  //         <section class="answer" id="answer-2">answer</section>
-  //         <br />
-  //         <section class="answer" id="answer-3">answer</section>
-  //         <br />
-  //         <section class="answer" id="answer-4">answer</section>
-  // });
+    newQuestion();
+  });
 }
-$("#start").on("click", function() {
+function ranChoice(max, min) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
+$("#start").on("click", function() {
   start();
 });
+
+function newQuestion() {
+  $(".inner-container").empty;
+  var newP = $("<p id='timer'>time remaining</p>");
+  newP.append(`<p> Question: ${response.results[counter].question} </p`);
+
+  newP.append(`<h3 class="answer" id="answer-1">${answer1}</h3>`);
+  newP.append(`<h3 class="answer" id="answer-2">${answer2}</h3>`);
+  newP.append(`<h3 class="answer" id="answer-3">${answer3}</h3>`);
+  newP.append(`<h3 class="answer" id="answer-4">${answer4}</h3>`);
+}
